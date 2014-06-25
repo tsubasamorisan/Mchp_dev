63cec61f56250facf498c2fd630fd8587360a603
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-06-24 19:53:32 -0700

    tweaks to js in verification email resend

--- mchp/user_profile/static/account/js/confirm-email.js
+++ mchp/user_profile/static/account/js/confirm-email.js
@@ -2,10 +2,6 @@ $(function(){
 	// to resend verification email
 	$("#resend_form").submit(function() {
 
-		// to change resend button state 
-		$("#sendBtn").addClass("btn-success");
-		$("#sendBtn").html("Verification E-mail sent!");
-
 		var url = "/login/resend-email/"; // page just for handing resend requests
 
 		$.ajax({
@@ -14,7 +10,9 @@ $(function(){
 			data: $("#resend_form").serialize(), 
 			dataType: "json",
 			success: function(data) {
-				$('.resend_confirm').html('<p>Verification Email resent.</p>');
+				// to change resend button state 
+				$("#sendBtn").addClass("btn-success");
+				$("#sendBtn").html("Verification E-mail sent!");
 			},
 			failure: function(data) {
 				alert('Resend request failed.');
