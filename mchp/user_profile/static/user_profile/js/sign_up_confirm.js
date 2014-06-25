2f99bd17b719d06bbacaded2ba6a7018d075580d
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-06-24 18:39:06 -0700

    show the social confirm page for testing purpose

--- mchp/user_profile/static/user_profile/js/sign_up_confirm.js
+++ mchp/user_profile/static/user_profile/js/sign_up_confirm.js
@@ -2,9 +2,9 @@ $(function() {
 	// automatically fill and submit the email field
 	hidden_email = $('.signup input[name=saved_email]').attr('value');
 	if(hidden_email !== '') {
-		$('body').hide();
+//		$('body').hide();
 		$('#id_email').attr('value', hidden_email);
 		$('#id_username').attr('value', 'charles'+Date.now());
-		$('.signup').submit();
+//		$('.signup').submit();
 	}
 });
