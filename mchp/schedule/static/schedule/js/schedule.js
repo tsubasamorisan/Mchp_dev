dfed01e03d75a304619843aa45c074dc21ec364b
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-06 21:28:17 -0700

    Gut static schedule files in favor of mchp ones

--- mchp/schedule/static/schedule/js/schedule.js
+++ mchp/schedule/static/schedule/js/schedule.js
@@ -3,34 +3,4 @@
  */
 
 $(function() {
-	/* messages */
-	$messages = $('.django-messages');
-	// fade messages that were added on page load
-	$messages.children('div').delay(5000).fadeOut(500, function(){
-		$(this).remove();
-	});
-
-	/* When messages are appended dynamically, they should fade out too */
-	// create an observer instance
-	var observer = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			var $nodes = $(mutation.addedNodes);
-			$nodes.delay(5000).fadeOut(500, function(){
-				$(this).remove();
-			});
-		});
-	});
-	// configuration of the observer:
-	var config = { attributes: true, childList: true, characterData: true };
-	// pass in the target node, as well as the observer options
-	observer.observe($messages.get(0), config);
-
-	/* custom scroll bar */
-	// can be applied to any div
-	$('.scrolls').enscroll({
-		showOnHover: false,
-		verticalTrackClass: 'track3',
-		verticalHandleClass: 'handle3',
-		scrollIncrement: 50,
-	});
 });
