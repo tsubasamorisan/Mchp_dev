45a0432e8fa504b2ab0c1b44fdeb9238d5e1f6df
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-08 15:49:41 -0700

    hopefully just fixed things

--- mchp/user_profile/static/user_profile/js/confirm-school.js
+++ mchp/user_profile/static/user_profile/js/confirm-school.js
@@ -1,4 +1,11 @@
 $(function(){
+	// show college select form when clicked
+	$('.show-college').on('click', function () {
+		$('.display-college').fadeOut(250, function () {
+			$('#college_select').fadeIn(500);
+		});
+	});
+	// populate college dropdown when we guess wrong (never!)
 	$('.dropdown-menu li').click(function(event) {
 		var $target = $(event.currentTarget);
 		var $drop = $('button.dropdown-toggle');
