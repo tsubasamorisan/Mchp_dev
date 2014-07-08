bca0808073420064997736eb6ffd00245171e9c7
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-08 14:15:16 -0700

    change to messages

---
+++ mchp/landing/static/landing/js/landing.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    // BS Validator 
+    $('#emailForm').bootstrapValidator({
+        fields: {
+            email: {
+                trigger: 'keyup',
+                validators: {
+                    notEmpty: {
+                        message: 'This field is required'
+                    },
+                    emailAddress: {
+                        message: 'Please enter a valid email address'
+                    },
+                    regexp: {
+                        regexp: /(\.edu)$/,
+                        message: 'Only .edu emails allowed'
+                    }
+                }
+            }
+        }
+    });
+});
