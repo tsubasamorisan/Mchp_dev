1f2198168264189c438d1423b67f077b78c9aa79
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-10 16:27:32 -0700

    Fix thumbnail bug for files with spaces in name

--- mchp/documents/models.py
+++ mchp/documents/models.py
@@ -16,9 +16,9 @@ import magic
 import logging
 logger = logging.getLogger(__name__)
 
-document_location = "documents/%Y/%m"
-thumbnail_location = "thumbnails"
-preview_location = "previews"
+DOCUMENT_LOCATION = "documents/%Y/%m"
+THUMBNAIL_LOCATION = "thumbnails"
+PREVIEW_LOCATION = "previews"
 
 def get_sentinel_course():
     school, created = School.objects.get_or_create(domain='deleted.edu', name='deleted')
@@ -30,20 +30,20 @@ def get_sentinel_course():
 
 class Document(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
+    description = models.CharField(max_length=1000)
     course = models.ForeignKey('schedule.Course', on_delete=models.SET(get_sentinel_course))
 
     up = models.IntegerField(default=0)
     down = models.IntegerField(default=0)
     price = models.PositiveIntegerField(default=0)
 
-    document = models.FileField(upload_to=document_location)
+    document = models.FileField(upload_to=DOCUMENT_LOCATION)
     md5sum = models.CharField(max_length=32)
     uuid = models.CharField(max_length=32)
     create_date = models.DateTimeField(auto_now_add=True)
 
-    thumbnail = models.ImageField(upload_to=thumbnail_location, blank=True, null=True)
-    preview = models.ImageField(upload_to=preview_location, blank=True, null=True)
+    thumbnail = models.ImageField(upload_to=THUMBNAIL_LOCATION, blank=True, null=True)
+    preview = models.ImageField(upload_to=PREVIEW_LOCATION, blank=True, null=True)
     slug = models.SlugField(max_length=80)
     
     def save(self, *args, **kwargs):
@@ -59,21 +59,13 @@ class Document(models.Model):
             self.slug = slugify(self.title)[:80]
             self.uuid = uuid.uuid4().hex
 
-            # add generated thumbnail filename
-            thumbnail = thumbnail_location + "/{}_thumb.png".format(
-                os.path.splitext(self.filename())[0]
-            )
-            self.thumbnail = thumbnail
-            # add generated preview filename
-            preview = preview_location + "/{}_preview.png".format(
-                os.path.splitext(self.filename())[0]
-            )
-            self.preview = preview
         super(Document, self).save(*args, **kwargs)
 
+    # if document.name is called before its actually saved, it will be different than the name on
+    # disk
     def filetype(self):
-        return magic.from_file(settings.MEDIA_ROOT + '/' + self.document.name,
-                       mime=True)
+        loc = settings.MEDIA_ROOT + '/' + self.document.name
+        return magic.from_file(loc, mime=True)
 
     def filename(self):
         return os.path.basename(self.document.name)
@@ -93,26 +85,40 @@ class Document(models.Model):
 #FIXME: woah
 @receiver(post_save, sender=Document)
 def create_thumbnail(sender, instance, **kwargs):
-    # first make sure dirs exist
-    os.makedirs(settings.MEDIA_ROOT + '/' + thumbnail_location, exist_ok=True)
-    os.makedirs(settings.MEDIA_ROOT + '/' + preview_location, exist_ok=True)
+    # don't do this more than once 
+    if not kwargs['created']:
+        return 
 
-    doc = Document.objects.get(pk=instance.pk)
+    # add generated thumbnail filename
+    thumbnail = THUMBNAIL_LOCATION + "/{}_thumb.png".format(
+        os.path.splitext(instance.filename())[0]
+    )
+    instance.thumbnail = thumbnail
+    # add generated preview filename
+    preview = PREVIEW_LOCATION + "/{}_preview.png".format(
+        os.path.splitext(instance.filename())[0]
+    )
+    instance.preview = preview
+    instance.save()
+
+    # first make sure dirs exist
+    os.makedirs(settings.MEDIA_ROOT + '/' + THUMBNAIL_LOCATION, exist_ok=True)
+    os.makedirs(settings.MEDIA_ROOT + '/' + PREVIEW_LOCATION, exist_ok=True)
+
     # word_types = ['application/doc', 'application/docx', 'application/text']
-    logger.debug(doc.filetype())
-    if doc.filetype() != b'application/pdf':
+    if instance.filetype() != b'application/pdf':
         return
 
     # first make thumbnail
     command = "convert -quality 95 -thumbnail 64 -background white\
             {}/{}[0] {}/{}".format(
-        settings.MEDIA_ROOT, doc.document, settings.MEDIA_ROOT, doc.thumbnail)
+        settings.MEDIA_ROOT, instance.document, settings.MEDIA_ROOT, instance.thumbnail)
     convert(command)
 
     # now make preview
     command = "convert -quality 95 -thumbnail 500 -background white\
             {}/{}[0] {}/{}".format(
-        settings.MEDIA_ROOT, doc.document, settings.MEDIA_ROOT, doc.preview)
+        settings.MEDIA_ROOT, instance.document, settings.MEDIA_ROOT, instance.preview)
     convert(command)
 
 def convert(command):
@@ -125,7 +131,7 @@ def convert(command):
         stderr=subprocess.PIPE,
     )
     stdout_value = proc.communicate()[0]
-    logger.debug(stdout_value)
+    logger.debug('stdout: ' + str(stdout_value))
 
 # Receive the pre_delete signal and delete the file associated with the model instance.
 @receiver(post_delete, sender=Document)
