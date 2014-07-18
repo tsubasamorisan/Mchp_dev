4338461874c80e48aae9ad520b4fe587825c97e0
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-17 21:03:36 -0700

    Fixes #2 Add confirmation to copy button

---
+++ mchp/documents/static/documents/js/documents.js
@@ -0,0 +1,10 @@
+$(function(){
+	// http or https needs to match the site requesting this resource
+	ZeroClipboard.config( { swfPath: "http://ajax.cdnjs.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.swf" } );
+
+	var client = new ZeroClipboard(document.getElementById("copy-button") );
+	client.on("aftercopy", function(){
+		// $('.copy-message').html("&#x2714; Link copied.").delay(2000).fadeOut(600);
+		$('.copy-message').html("&#x2714; Link copied.");
+	});
+});
--- mchp/documents/static/documents/js/view.js
+++ mchp/documents/static/documents/js/view.js
@@ -1,8 +1,4 @@
 $(function(){
-	// http or https needs to match the site requesting this resource
-	ZeroClipboard.config( { swfPath: "http://ajax.cdnjs.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.swf" } );
-	var client = new ZeroClipboard(document.getElementById("copy-button") );
-
 	// ajax stuff
 	var csrftoken = $.cookie('csrftoken');
 	function csrfSafeMethod(method) {
