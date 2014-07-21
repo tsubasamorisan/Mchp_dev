4bb5b6db9cda3fd1ce67c9fde2366d65570b53b7
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-20 17:18:25 -0700

    Share button should work properly on preview page

--- mchp/documents/static/documents/js/documents.js
+++ mchp/documents/static/documents/js/documents.js
@@ -9,4 +9,42 @@ $(function(){
 			$('.copy-message').html("&#x2714; Link copied.");
 		});
 	}
+	/* fb stuff */
+	window.fbAsyncInit = function(){
+		FB.init({
+			appId: '1488225931413638',
+		status: true,
+		cookie: true,
+		xfbml: true 
+		}); 
+	};
+	(function(d, debug) {
+		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+		if (d.getElementById(id)) {
+			return;
+		}
+		js = d.createElement('script'); 
+		js.id = id;
+		js.async = true;
+		js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+		ref.parentNode.insertBefore(js, ref);
+	}(document, /*debug*/ true));
+
+	function postToFeed(title, desc, url, image){
+		var obj = {
+			method: 'feed',
+			link: url, 
+			picture: image,
+			name: title,
+			description: desc
+		};
+		function callback(response){}
+		FB.ui(obj, callback);
+	}
+	$('.btnShare').click(function(){
+		elem = $(this);
+		postToFeed(elem.data('title'), elem.data('desc'), elem.prop('href'), elem.data('image'));
+
+		return false;
+	});
 });
