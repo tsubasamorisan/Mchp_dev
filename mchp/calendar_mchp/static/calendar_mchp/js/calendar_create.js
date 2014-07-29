caaf2d8f039d739155c626c85e6fbf47658678c1
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-29 16:20:12 -0700

    set am/pm option for clockpicker

--- mchp/calendar_mchp/static/calendar_mchp/js/calendar_create.js
+++ mchp/calendar_mchp/static/calendar_mchp/js/calendar_create.js
@@ -47,7 +47,9 @@ $(function() {
 	});
 
 	// initializing clockpicker
-	$('.clockpicker').clockpicker();
+	$('.clockpicker').clockpicker( {
+		twelvehour: 'true'
+	});
 
    	// when the meeting days button option changes
    	$("#mon").on('click', function() {
