a09aeaae4d3a13d046b7fe3e0836adbc0cd0906e
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-07-29 15:25:08 -0700

    editing the events add page

--- mchp/calendar_mchp/static/calendar_mchp/js/events_add.js
+++ mchp/calendar_mchp/static/calendar_mchp/js/events_add.js
@@ -3,25 +3,33 @@
  *
  * This file handles the functionality of the calendar events add page and bootstrap xeditable
  */
-$(function() {
+ $(function() {
+    //display dropdown selection 
     $(".dropdown-menu li a").click(function(){
-    //display dropdown selection in the button
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text()).append(" <span class='caret'></span>");
-      //fade in course info and the template on selection
-      $('.course-info').fadeIn(500).removeClass('hidden');
-      $('.event-template').fadeIn(700).removeClass('hidden');
+        //display the selected calendar in the button
+        $(".btn:first-child").text($(this).text());
+        $(".btn:first-child").val($(this).text()).append(" <span class='caret'></span>");
+        //bring the course info and template into view once calendar is selected
+        $('.course-info').fadeIn(500).removeClass('hidden');
+        $('.event-template').fadeIn(700).removeClass('hidden');
+
     });
 
-	//iniitiating bs xeditable
+    //turn to inline mode
+    $.fn.editable.defaults.mode = 'inline';
+    //make editable field empty 
+    $.fn.editable.defaults.emptytext = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+
+    // initiate xeditable 
     $(".editField").editable({
-       type: "text",
-       showbuttons: false,
-       anim: "fast"    
-	    //pk: 1,
-	    //url: '/post',   
+        type: "text",
+        showbuttons: false,
+        anim: "fast"    
+
+        //pk: 1,
+        //url: '/post',   
     });
-    // due date drop down- we probably need times in there too
+    // provide due date moments, we probably need times as well
     $('.due').editable({
         value: 1, 
         showbuttons: false,
@@ -31,7 +39,7 @@ $(function() {
         {value: 3, text: 'By Midnight'}
         ]
     });
-    // make editable go to next field in row on enter, can't get it to go to the next row though
+    // tried to get it to switch to the next table data on "enter", but couldnt get it to go to the next row
     $('.editable').on('hidden', function(e, reason){
         if(reason === 'save' || reason === 'nochange') {
             var $next = $(this).closest('td').next().find('.editable');
@@ -40,10 +48,8 @@ $(function() {
             }, 300);
         }
     });
-    //turn to inline mode
-    $.fn.editable.defaults.mode = 'inline';
-    //make empty text almost display as blank
-    $.fn.editable.defaults.emptytext = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+
+
 
 });
 
