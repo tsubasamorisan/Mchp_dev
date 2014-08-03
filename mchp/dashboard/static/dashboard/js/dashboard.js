cccce1f6b0db7c5f3432efabcc4cf29de98c1c5d
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-08-03 12:25:27 -0700

    implement news sections and other touch ups

--- mchp/dashboard/static/dashboard/js/dashboard.js
+++ mchp/dashboard/static/dashboard/js/dashboard.js
@@ -1,9 +1,16 @@
 $(function(){
+	//make breadcrumbs sortable
 	$(".breadcrumb").sortable ({ 
 		cancel: '#weather_data',
 		placeholder: 'ql-placeholder',
 		containment: ".breadcrumb",
 		scroll: false
 	});
+	//make a breadcrumb not clickable when dragging
 	$( ".breadcrumb" ).disableSelection();
+
+	//toggle news categories section
+	$('#edit_sections').on('click', function () {
+    	$('.flip-holder').toggleClass("flip");
+	});
 });
