d5ad8d94b388693d69ae780fa2bb1e0a139cba27
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-08-18 14:05:46 -0700

    Ordery schools alpha on ql admin page

--- mchp/schedule/admin.py
+++ mchp/schedule/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.db.models import get_models, get_app
+from django import forms
+
+from schedule.models import SchoolQuicklink, School
 
 for model in get_models(get_app('schedule')):
+    if model == SchoolQuicklink:
+        continue
     admin.site.register(model)
+
+class SchoolQuicklinkAdminForm(forms.ModelForm):
+    domain = forms.ModelChoiceField(queryset=School.objects.order_by('name'))
+
+    class Meta:
+        model = SchoolQuicklink
+
+class SchoolQuickLinkAdmin(admin.ModelAdmin):
+    form = SchoolQuicklinkAdminForm
+
+admin.site.register(SchoolQuicklink, SchoolQuickLinkAdmin)
