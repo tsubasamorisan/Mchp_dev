23929e30eca0bacb09a8eb8b5d44628747544488
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-08-28 20:03:46 -0700

    Fixes #52 Improve info about failed doc upload

--- mchp/documents/views.py
+++ mchp/documents/views.py
@@ -146,7 +146,7 @@ class DocumentListView(ListView):
             # can't filter on annotations so get the count manually
             'review_count': 'SELECT COUNT(*) FROM "documents_documentpurchase"'+\
             'WHERE ("documents_documentpurchase"."document_id" = "documents_document"."id"'+\
-            'AND NOT ("documents_documentpurchase"."review_date" IS NULL))'
+            'AND NOT ("documents_documentpurchase"."review_date" IS NULL))',
         })
 
     def get_context_data(self, **kwargs):
@@ -162,6 +162,7 @@ class DocumentListView(ListView):
             'AND NOT ("documents_documentpurchase"."review_date" IS NULL))'
         })
         context['purchases'] = purchases
+        context['now'] = timezone.now()
 
         return context
 
