c4aad4aa2bf1574c72f35f8c5fa99c50dc93945c
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-09-01 19:50:02 +0000

    zeroclip https

--- mchp/documents/static/documents/js/documents.js
+++ mchp/documents/static/documents/js/documents.js
@@ -1,7 +1,7 @@
 $(function(){
 	// http or https needs to match the site requesting this resource
 	if (typeof ZeroClipboard != 'undefined') {
-		ZeroClipboard.config( { swfPath: "http://ajax.cdnjs.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.swf" } );
+		ZeroClipboard.config( { swfPath: "https://ajax.cdnjs.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.swf" } );
 
 		var client = new ZeroClipboard(document.getElementById("copy-button") );
 		client.on("aftercopy", function(){
