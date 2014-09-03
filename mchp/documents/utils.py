26a64a40f887f03ced90a0bf7d08a54acfac85cc
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-09-03 02:58:28 +0000

    more hot fixes

--- mchp/documents/utils.py
+++ mchp/documents/utils.py
@@ -16,6 +16,40 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
+def reupload_doc():
+    cursor = connection.cursor()
+    cursor.execute("select * from old_doc order by id")
+    docs = dictfetchall(cursor)
+
+    base_url = 'https://mchpstore.s3.amazonaws.com/'
+    for doc in docs:
+        old_id = doc['id']
+        cursor.execute('select id from documents_document where old_id = %s', [old_id])
+        document = cursor.fetchone()
+        if document:
+            new_id = document[0]
+            document = Document.objects.filter(
+                id = new_id,
+            )[0]
+        else: 
+            continue
+        print(document.preview)
+        continue
+        url = base_url+"{}".format(doc['path'])
+
+        r = requests.get(url, stream=True)
+        doc_name = os.path.basename(doc['name'])
+        filename = '/tmp/{}.pdf'.format(doc_name)
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    f.flush()
+        f = open(filename, 'rb')
+        new_file = File(f)
+        document.document = new_file
+        print('found ' + document.title)
+        document.save()
 
 def upload_doc():
     cursor = connections['production'].cursor()
