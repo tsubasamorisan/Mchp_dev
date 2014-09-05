5db03e240051361932cc2a306b1cb67e27e5ec19
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-09-05 00:21:11 -0700

    add student to user in admin page

--- mchp/user_profile/admin.py
+++ mchp/user_profile/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.db.models import get_models, get_app
 
+from user_profile.models import Student, UserRole
+
 for model in get_models(get_app('user_profile')):
+    if model == Student:
+        continue
     admin.site.register(model)
+
+class StudentAdmin(admin.ModelAdmin):
+    exclude = ('friends',)
+
+admin.site.register(Student, StudentAdmin)
+
+class StudentInline(admin.StackedInline):
+    model = Student
+    exclude = ('friends', 'school', 'major')
+
+class UserRoleInline(admin.StackedInline):
+    model = UserRole
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [
+        StudentInline,
+        UserRoleInline,
+    ]
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
