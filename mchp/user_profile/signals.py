9f8d0fc6f457746b545b53a389013d45b87c874e
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-09-05 17:02:33 -0700

    notify me when log in

--- mchp/user_profile/signals.py
+++ mchp/user_profile/signals.py
@@ -1,3 +1,23 @@
 from django.dispatch import Signal
+from django.dispatch.dispatcher import receiver
+from django.contrib.auth.models import User
+
+from allauth.account.signals import user_logged_in
+
+from notification.api import add_notification
 
 enrolled = Signal(providing_args=['enroll'])
+
+@receiver(user_logged_in)
+def log_in_notify(sender, request, user, **kwargs):
+    me = User.objects.filter(
+        username = 'Adapt',
+    )
+    if me.exists():
+        me = me[0]
+    else:
+        return
+    add_notification(
+        me,
+        user.username + ' has logged in'
+    )
