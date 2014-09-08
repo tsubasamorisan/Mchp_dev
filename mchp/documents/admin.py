f0f60238bde591fdeda348ca38ef39642df2f2c8
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-09-08 04:18:06 -0700

    made document in admin more friendly

--- mchp/documents/admin.py
+++ mchp/documents/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.db.models import get_models, get_app
 from django import forms
 
-from documents.models import Document
+from documents.models import Document, Upload
 from documents.widgets.admin import AdminDocThumnailWidget
 
 for model in get_models(get_app('documents')):
-    if model == Document:
+    if model == Document or model == Upload:
         continue
     admin.site.register(model)
 
@@ -19,7 +19,23 @@ class DocumentAdminForm(forms.ModelForm):
             'preview' : AdminDocThumnailWidget(),
         }
 
+class DocumentUploadInline(admin.StackedInline):
+    model = Upload
+
 class DocumentAdmin(admin.ModelAdmin):
     form = DocumentAdminForm
+    search_fields = ['title']
+    readonly_fields = ['uuid', 'md5sum', 'filetype']
+    inlines = [
+        DocumentUploadInline,
+    ]
 
 admin.site.register(Document, DocumentAdmin)
+
+class UploadAdmin(admin.ModelAdmin):
+    search_fields = [
+        'document__title', 
+        'owner__user__username',
+    ]
+
+admin.site.register(Upload, UploadAdmin)
