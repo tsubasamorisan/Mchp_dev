56ed15fa29fbbb344a6e24b57751cf125998cba8
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-09-23 18:44:51 -0700

    add more to user on admin page

--- mchp/user_profile/admin.py
+++ mchp/user_profile/admin.py
@@ -3,6 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.db.models import get_models, get_app
 
+from allauth.account.models import EmailAddress
+
 from user_profile.models import Student, UserRole
 
 for model in get_models(get_app('user_profile')):
@@ -17,15 +19,20 @@ admin.site.register(Student, StudentAdmin)
 
 class StudentInline(admin.StackedInline):
     model = Student
-    exclude = ('friends', 'school', 'major')
+    readonly_fields = ['school', 'major', 'friends']
 
 class UserRoleInline(admin.StackedInline):
     model = UserRole
+
+class EmailInline(admin.TabularInline):
+    model = EmailAddress
+    extra = 0
 
 class CustomUserAdmin(UserAdmin):
     inlines = [
         StudentInline,
         UserRoleInline,
+        EmailInline,
     ]
     list_filter = UserAdmin.list_filter + ('date_joined',)
 
