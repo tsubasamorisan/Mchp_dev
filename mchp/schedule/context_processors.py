0e67673907f950c6cadd1a594b529ad5aadec8b9
tsubasamorisan <tsubasamori.eros@gmail.com>
2014-10-01 13:06:13 -0700

    add context procs. for school on pages

---
+++ mchp/schedule/context_processors.py
@@ -0,0 +1,21 @@
+def school(request):
+    class no_school():
+        def __init__(self):
+            self.pk = 0
+        def __str__(self):
+            return 'Not a school'
+
+    no_school_context = {
+        'school': no_school()
+    }
+
+    if request.user.is_anonymous():
+        return no_school_context
+    if not request.user.student_exists():
+        return no_school_context
+
+    student = request.user.student
+    context = {
+        'school': student.school,
+    }
+    return context
