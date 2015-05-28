80853fc0c85e217419d8cf1d0e06b2d65418a860
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-05-28 16:53:07 -0700

    Fix typo

--- mchp/studyguides/views.py
+++ mchp/studyguides/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import get_object_or_404
-from django.http.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 from .models import StudyGuideCampaignSubscriber
 from .utils import unsubscribe_student, resubscribe_student
 
