6ed26205ad2d58ea35cb47cfa3a7d8c216599642
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-05-28 17:26:38 -0700

    Update mgmt command

--- mchp/studyguides/views.py
+++ mchp/studyguides/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render_to_response
-from .models import StudyGuideCampaignSubscriber
+from campaigns.models import CampaignSubscriber
+from .models import StudyGuideAnnouncement
 from .utils import unsubscribe_student, resubscribe_student
 
 
@@ -7,19 +8,24 @@ def unsubscribed(request, uuid):
     """ Subscriber unsubscribed from an e-mail.
 
     """
-    subscriber = get_object_or_404(StudyGuideCampaignSubscriber, uuid=uuid)
+    subscriber = get_object_or_404(CampaignSubscriber, uuid=uuid)
     unsubscribe_student(subscriber)
+
+    announcement = StudyGuideAnnouncement.objects.get(
+        campaign=subscriber.campaign)
     return render_to_response('studyguides/unsubscribed_page.html', {
         'subscriber': subscriber,
-        'course': subscriber.campaign.event.calendar.course})
+        'course': announcement.event.calendar.course})
 
 
 def resubscribed(request, uuid):
     """ Subscriber unsubscribed from an e-mail.
 
     """
-    subscriber = get_object_or_404(StudyGuideCampaignSubscriber, uuid=uuid)
+    subscriber = get_object_or_404(CampaignSubscriber, uuid=uuid)
     resubscribe_student(subscriber)
+    announcement = StudyGuideAnnouncement.objects.get(
+        campaign=subscriber.campaign)
     return render_to_response('studyguides/resubscribed_page.html', {
         'subscriber': subscriber,
-        'course': subscriber.campaign.event.calendar.course})
+        'course': announcement.event.calendar.course})
