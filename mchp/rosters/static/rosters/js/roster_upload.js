997d7c2c2b63128fd29d18c50dc51997c011bf0a
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-06-11 16:50:52 -0700

    x-editable now works

---
+++ mchp/rosters/static/rosters/js/roster_upload.js
@@ -0,0 +1,45 @@
+/*
+ * upload_roster.js
+ * 
+ * This file is for the roster upload page
+ */
+
+$(function() {
+
+	// Bootstrap Editable form fields
+
+	// make the newly added fields editable
+	$(".editField").editable({
+		type: "text",
+		mode: "inline",
+		showbuttons: false,
+		anim: "fast",
+		onblur: 'cancel'  
+	});
+
+	$('.examSelect').editable({
+		type: "select",
+		mode: "inline",
+		showbuttons: false,
+		anim: "fast",
+		onblur: 'cancel', 
+        value: 1,    
+        source: [
+              {value: 1, text: 'Exam 1'},
+              {value: 2, text: 'Exam 2'},
+              {value: 3, text: 'Exam 3'}
+           ]
+    });
+
+    $('.rosterCode').editable({
+        type: "textarea",
+        inputclass: "input-sm",
+        placeholder: "Command + v here to paste",
+		mode: "inline",
+		showbuttons: false,
+		anim: "fast",
+		onblur: 'cancel',
+        rows: 1
+    });
+		
+});
