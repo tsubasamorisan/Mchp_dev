5f8e4044b25dad677faedf290dca52d73dfa5ba3
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-07-23 17:09:54 -0700

    fix merge conflict

--- mchp/rosters/urls.py
+++ mchp/rosters/urls.py
@@ -3,13 +3,9 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',  # noqa
-<<<<<<< HEAD
-    url(r'^add/$', views.RosterCreate.as_view(), name='roster-upload'),
-=======
     url(r'^add/$', views.RosterSubmitView.as_view(), name='roster-upload'),
 
     url(r'^review/(?P<pk>\d+)/$',
         views.RosterReviewView.as_view(),
         name='roster-review'),
->>>>>>> fb3334ddd3a28741912fc30e5ab45a59d56c00cd
 )
