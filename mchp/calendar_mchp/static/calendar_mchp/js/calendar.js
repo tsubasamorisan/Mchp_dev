b785b36710933b56fdaf8823e106f8427ae5dd05
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-08-31 13:53:27 -0700

    style changes to cal

--- mchp/calendar_mchp/static/calendar_mchp/js/calendar.js
+++ mchp/calendar_mchp/static/calendar_mchp/js/calendar.js
@@ -956,16 +956,16 @@ var drawCircle = function(canvas, old) {
 
 		ctx.beginPath();
 		var x              = $(canvas).width() / 2;               // x coordinate
-		var y              = $(canvas).height() / 2;               // y coordinate
-		var radius         = $(canvas).width() / 4.5;                    // Arc radius
-		var startAngle     = 0;                     // Starting point on circle
-		var endAngle       = Math.PI * 2; // End point on circle
-		var anticlockwise  = true; // clockwise or anticlockwise
+		var y              = $(canvas).height() / 2;              // y coordinate
+		var radius         = $(canvas).width() / 4.5;             // Arc radius
+		var startAngle     = 0;                     							// Starting point on circle
+		var endAngle       = Math.PI * 2; 												// End point on circle
+		var anticlockwise  = true; 																// clockwise or anticlockwise
 
 		ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
 
-		var color = "#4C9ED9";
-		var stroke = "#357EBD";
+		var color = "rgba(255,255,255,0.4)";
+		var stroke = "#FFFFFF";
 		if (old) {
 			ctx.globalAlpha = 0.4;
 			stroke = 'rgba(0,0,0,0)';
