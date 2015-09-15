6ce31953b40b5e680475182e8269d63b3e372e50
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-09-15 11:53:05 -0700

    re-structure how docs are displayed on course/school pages using tables

--- mchp/schedule/static/schedule/js/school.js
+++ mchp/schedule/static/schedule/js/school.js
@@ -4,4 +4,6 @@
  * This file handles functionality for a public school page
  */
 
-
+$(function () {
+  $( 'input').attr('placeholder','Search Documents by Title, Price, Instructor...').addClass('input-lg');
+});
