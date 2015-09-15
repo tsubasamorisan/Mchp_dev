228621269631267bdcbc55f09dc682253930a161
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-09-15 12:11:43 -0700

    pushing test merge migration

---
+++ mchp/user_profile/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user_profile', '0004_student_created_by_roster_no_user'),
+        ('user_profile', '0004_userrole_intern_manager'),
+    ]
+
+    operations = [
+    ]
