ed204e3c3ca550e96a2a9d791e62946c2b4632e4
tsubasamorisan <tsubasamori.eros@gmail.com>
2015-09-22 00:18:39 +0200

    Just throwing stuff and seeing what sticks

--- mchp/studyguides/utils.py
+++ mchp/studyguides/utils.py
@@ -134,7 +134,7 @@ def _rank_documents(event):
                    lambda doc: doc.rating(),
                    score=10)
 
-    print('DEBUG: SCORES = ' + str(scores[0]))
+    print('DEBUG: SCORES = ' + str(scores[1]))
     top_score = scores.most_common(1)
     if top_score:
         top_score = top_score[0][1]
